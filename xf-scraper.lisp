fc51279211fde17987decdce263574316ff5bca9
ChristopherSegale <ccs.segale@gmail.com>
2020-07-02 13:40:10 -0400

    Removing superfluous let expression in main function.

--- xf-scraper.lisp
+++ xf-scraper.lisp
@@ -4,7 +4,7 @@
 
 (in-package :xf-scraper)
 
-(defmacro with-page-body (url &rest body)
+(defmacro with-page-body (url &body body)
   (let ((request (gensym)))
     `(let* ((,request (dex:get ,url)) (page-body (lquery:$ (initialize ,request))))
        ,@body)))
@@ -20,14 +20,14 @@
      (lambda () post-author)
      (lambda () post-content))))
 
-(defmacro with-posts (pb &rest body)
+(defmacro with-posts (pb &body body)
   `(let ((posts (map 'list #'(lambda (a p) (make-post a p)) ;;creating post list
 		     (remove-if #'null (lquery:$ ,pb "article" (attr :data-author))) ;;getting author list
 		     (map 'list #'(lambda (p) (elt (lquery:$ (inline (concatenate 'string "#" p)) "article" (text)) 0)) ;;getting post content list
 			  (remove-if #'null (lquery:$ ,pb "article" (attr :id))))))) ;;getting post id list
      ,@body))
 
-(defmacro check-page (url &rest body)
+(defmacro check-page (url &body body)
   `(if ,url
        (with-page-body ,url
 	 (with-posts page-body
@@ -35,6 +35,5 @@
        (format t "~A needs first argument to be an url.~%" (uiop:argv0))))
 
 (defun main ()
-  (let ((url (car (uiop:command-line-arguments))))
-    (check-page url
-		(mapc #'(lambda (p) (princ (funcall p))) posts))))
+  (check-page (car (uiop:command-line-arguments))
+		(mapc #'(lambda (p) (princ (funcall p))) posts)))
