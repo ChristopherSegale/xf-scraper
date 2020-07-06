029b8da18f1c72349a7603564e9193bc71cee45a
ChristopherSegale <ccs.segale@gmail.com>
2020-07-05 20:37:30 -0400

    Refactoring code and adding documentation strings.

--- xf-scraper.lisp
+++ xf-scraper.lisp
@@ -4,15 +4,22 @@
 
 (in-package :xf-scraper)
 
-(defmacro with-page-body (url &body body)
-  (let ((request (gensym)))
-    `(let* ((,request (dex:get ,url)) (page-body (lquery:$ (initialize ,request))))
-       ,@body)))
+(declaim (inline get-page)
+	 (inline remove-whitespace))
+
+(defun get-page (url)
+  "Get database which the posts are pulled from."
+  (let ((request (dex:get url)))
+    (lquery:$ (initialize request))))
+
+(declaim (inline remove-whitespace))
 
 (defun remove-whitespace (str)
+  "Remove whitespace from beginning and end of string."
   (string-trim '(#\Space #\Newline #\Backspace #\Tab #\Linefeed #\Page #\Return #\Rubout) str))
 
 (defun make-post (author post)
+  "Returns a function which prints the contents of a post."
   (let ((post-author author)
 	(post-content (remove-whitespace post)))
     (values
@@ -20,19 +27,31 @@
      (lambda () post-author)
      (lambda () post-content))))
 
+(defmacro with-gensyms (vars &body body)
+  "Binds a list of variables with gensym values."
+  (flet ((get-bind (va)
+	   (mapcar #'(lambda (v) `(,v (gensym))) va)))
+	 `(let (,@(get-bind vars))
+	    ,@body)))
+
 (defmacro with-posts (pb &body body)
-  `(let ((posts (map 'list #'make-post ;;creating post list
-		     (remove-if #'null (lquery:$ ,pb "article" (attr :data-author))) ;;getting author list
-		     (map 'list #'(lambda (p) (elt (lquery:$ (inline (concatenate 'string "#" p)) "article" (text)) 0)) ;;getting post content list
-			  (remove-if #'null (lquery:$ ,pb "article" (attr :id))))))) ;;getting post id list
-     ,@body))
+  "Anaphoric macro which automatically uses the 'posts symbol to hold a post list."
+  (with-gensyms (page)
+    `(let* ((,page ,pb)
+	    (posts (map 'list #'make-post ;;creating post list
+			(remove-if #'null (lquery:$ ,page "article" (attr :data-author))) ;;getting author list
+			(map 'list #'(lambda (p) (elt (lquery:$ (inline (concatenate 'string "#" p)) "article" (text)) 0)) ;;getting post content list
+			     (remove-if #'null (lquery:$ ,page "article" (attr :id))))))) ;;getting post id list
+       ,@body)))
 
 (defmacro check-page (url &body body)
-  `(if ,url
-       (with-page-body ,url
-	 (with-posts page-body
-	   ,@body))
-       (format t "~A needs first argument to be an url.~%" (uiop:argv0))))
+  "Wraps the with-posts macro in a conditional which checks if a url is given"
+  (with-gensyms (link)
+    `(let ((,link ,url))
+       (if ,link
+	   (with-posts (get-page ,link)
+	     ,@body)
+       (format t "~A needs first argument to be an url.~%" (uiop:argv0))))))
 
 (defun main ()
   (check-page (car (uiop:command-line-arguments))
